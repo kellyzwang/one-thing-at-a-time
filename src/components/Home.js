defed851f88fb010c432dd854f6d8b091c5e3233
Kelly <zwang28@uw.edu>
2022-08-26 13:14:45 -0700

    fixed home css

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,20 +11,7 @@ export function Home() {
         <h2>Focus on one thing at a time, get more done, and feel less stressed.</h2>
       </section>
 
-      <div id="rabbit-row">
-       <img src='img/light-brown-rabbit.png' alt='light-brown-rabbit logo'></img>
-       <img src='img/green-rabbit.png' alt='green-rabbit logo'></img>
-       <img src='img/dark-brown-rabbit.png' alt='dark-brown-rabbit logo'></img>
-       <img src='img/white-rabbit.png' alt='white-rabbit logo'></img>
-       <img src='img/pink-rabbit.png' alt='pink-rabbit logo'></img>
-       <img src='img/red-rabbit.png' alt='red-rabbit logo'></img>
-       <img src='img/light-brown-rabbit.png' alt='light-brown-rabbit logo'></img>
-       <img src='img/green-rabbit.png' alt='green-rabbit logo'></img>
-       <img src='img/dark-brown-rabbit.png' alt='dark-brown-rabbit logo'></img>
-       <img src='img/white-rabbit.png' alt='white-rabbit logo'></img>
-       <img src='img/pink-rabbit.png' alt='pink-rabbit logo'></img>
-       <img src='img/red-rabbit.png' alt='red-rabbit logo'></img>
-       <img src='img/light-brown-rabbit.png' alt='light-brown-rabbit logo'></img>
+     <div id="rabbit-row" className="repeatimg">
      </div>
    </div>
 
